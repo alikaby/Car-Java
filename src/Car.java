390084962347de96941970d9678ab4571f3422e6
Alexandra Bystrova <saobyjob90@gmail.com>
2022-02-03 12:51:10 +0300

    First commit

---
+++ src/Car.java
@@ -0,0 +1,20 @@
+ import java.util.Scanner;
+ public class Car {
+    public static void build() {
+        Scanner scan = new Scanner(System.in);
+         a = scan.next();
+
+
+        System.out.println("You must build a new car");
+    }
+
+    public static void buy() {
+        System.out.println("I will buy this car");
+    }
+    int carSpeed = 0;
+    public int lol (){
+        System.in.read();
+    }
+}
+
+
